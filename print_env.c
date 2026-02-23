7a173b600ba045443d64c40a52ad7fd72a7fd324
Eljan Badalov <123768216+EljanBadalov@users.noreply.github.com>
2026-02-23 12:08:04 +0400

    Update print_env.c

--- print_env.c
+++ print_env.c
@@ -1,7 +1,14 @@
+/**
+ * File: print_env.c
+ * Auth: Eljan Badalov
+ * Desc: Prints all environment variables
+ */
 #include "main.h"
 /**
-  * print_env - print env variables
-  */
+ * print_env - prints the environment variables
+ *
+ * Return: nothing
+ */
 void print_env(void)
 {
 	int i = 0;
